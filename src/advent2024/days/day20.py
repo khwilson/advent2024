c9b70e7c8b062e3a0bede5d351d74c0a02811005
Kevin H Wilson <khwilson@gmail.com>
2024-12-23 22:33:41 -0500

    Day 20 Part 2

--- src/advent2024/days/day20.py
+++ src/advent2024/days/day20.py
@@ -23,15 +23,18 @@ def four_ways(x: int, y: int, max_x: int, max_y: int) -> list[tuple[int, int]]:
     return out
 
 
-def twenty_ways(x: int, y: int, max_x: int, max_y: int) -> list[tuple[int, int]]:
+def twenty_ways(x: int, y: int, max_x: int, max_y: int) -> list[tuple[int, int, int]]:
     out: list[tuple[int, int]] = []
     for i in range(-20, 21):
         max_j = 20 - abs(i)
         for j in range(-max_j, max_j + 1):
             xx = x + i
             yy = y + j
+            dist = abs(i) + abs(j)
+            if dist == 0:
+                continue
             if 0 <= xx < max_x and 0 <= yy < max_y:
-                out.append((xx, yy))
+                out.append((xx, yy, dist))
     return out
 
 
@@ -64,7 +67,6 @@ def part1(data_file: str | Path, is_part_two: bool = False) -> int | str:
                 dist_to_end[(i, j)] = length + 1
                 queue.append(((i, j), length + 1))
 
-
     # Compute all distances from `start` to every other point`
     dist_from_start: dict[tuple[int, int], int] = {start: 0}
     queue = deque([(start, 0)])
@@ -81,15 +83,23 @@ def part1(data_file: str | Path, is_part_two: bool = False) -> int | str:
     savings: dict[tuple[int, int], int] = defaultdict(int)
     for row in tqdm(range(1, height - 1)):
         for col in range(1, width - 1):
-            if maze[i][j] != "#":
+            if maze[row][col] != "#":
                 continue
 
             xx = four_ways(row, col, height, width)
             for (lx, ly), (rx, ry) in its.combinations(xx, 2):
                 if maze[lx][ly] != "#" and maze[rx][ry] != "#":
                     # import ipdb; ipdb.set_trace()
-                    savings[(row, col)] = max(savings[(row, col)], the_dist - (dist_from_start[(lx, ly)] + dist_to_end[(rx, ry)] + 1))
-                    savings[(row, col)] = max(savings[(row, col)], the_dist - (dist_to_end[(lx, ly)] + dist_from_start[(rx, ry)] + 1))
+                    savings[(row, col)] = max(
+                        savings[(row, col)],
+                        the_dist
+                        - (dist_from_start[(lx, ly)] + dist_to_end[(rx, ry)] + 2),
+                    )
+                    savings[(row, col)] = max(
+                        savings[(row, col)],
+                        the_dist
+                        - (dist_to_end[(lx, ly)] + dist_from_start[(rx, ry)] + 2),
+                    )
 
     return sum(1 for val in savings.values() if val >= 100)
 
@@ -123,7 +133,6 @@ def part2(data_file: str | Path) -> int | str:
                 dist_to_end[(i, j)] = length + 1
                 queue.append(((i, j), length + 1))
 
-
     # Compute all distances from `start` to every other point`
     dist_from_start: dict[tuple[int, int], int] = {start: 0}
     queue = deque([(start, 0)])
@@ -140,13 +149,15 @@ def part2(data_file: str | Path) -> int | str:
     savings: dict[tuple[int, int], int] = defaultdict(int)
     for row in tqdm(range(1, height - 1)):
         for col in range(1, width - 1):
-            if maze[i][j] != "#":
+            if maze[row][col] == "#":
                 continue
 
-            xx = four_ways(row, col, height, width)
-            for (lx, ly), (rx, ry) in its.combinations(xx, 2):
-                if maze[lx][ly] != "#" and maze[rx][ry] != "#":
-                    savings[(row, col)] = max(savings[(row, col)], the_dist - (dist_from_start[(lx, ly)] + dist_to_end[(rx, ry)] + 1))
-                    savings[(row, col)] = max(savings[(row, col)], the_dist - (dist_to_end[(lx, ly)] + dist_from_start[(rx, ry)] + 1))
+            for l, r, d in twenty_ways(row, col, height, width):
+                if maze[l][r] != "#":
+                    savings[(row, col, l, r)] = max(
+                        savings[(row, col, l, r)],
+                        the_dist
+                        - (dist_from_start[(row, col)] + dist_to_end[(l, r)] + d),
+                    )
 
     return sum(1 for val in savings.values() if val >= 100)
