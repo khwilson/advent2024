acd12f822bf4983b118388dbe4970eeddead4ed9
Kevin H Wilson <khwilson@gmail.com>
2024-12-23 23:03:47 -0500

    Day 23 Part 2

--- src/advent2024/days/day23.py
+++ src/advent2024/days/day23.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
+from functools import cache
 from pathlib import Path
 
 
 def read_data(data_file: str | Path) -> list[set[str]]:
     with open(data_file, "rt") as infile:
         return [frozenset(line.strip().split("-")) for line in infile]
-
 
 
 def part1(data_file: str | Path) -> int | str:
@@ -27,9 +27,32 @@ def part1(data_file: str | Path) -> int | str:
                     seen.add(the_set)
     return len(seen)
 
-def part2(data_file: str | Path) -> int | str:
-    data = read_data(data_file)
 
+def part2(data_file: str | Path) -> int | str:
     # OK, so they want us to do max clique. Gotta love a good NP-complete
     # problem late in time!
-    return 10
+
+    data = read_data(data_file)
+    adj_graph: dict[str, set[str]] = defaultdict(set)
+    for l, r in data:
+        adj_graph[l].add(r)
+        adj_graph[r].add(l)
+
+    adj_graph = {key: frozenset(val) for key, val in adj_graph.items()}
+
+    @cache
+    def rec(clique: frozenset[str], vertices: frozenset[str]) -> frozenset[str]:
+        max_clique = clique
+        for vertex in vertices - clique:
+            if clique <= adj_graph[vertex]:
+                next_clique = rec(clique | {vertex}, vertices & adj_graph[vertex])
+                if len(next_clique) > len(max_clique):
+                    max_clique = next_clique
+
+        return max_clique
+
+    return ",".join(
+        sorted(
+            max((rec(frozenset([key]), val) for key, val in adj_graph.items()), key=len)
+        )
+    )
